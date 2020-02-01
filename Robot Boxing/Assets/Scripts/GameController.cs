3bce9806da2bef463ad405298ccb2abc9f631cd2
z99o <z99o@users.noreply.github.com>
2020-02-01 08:37:19 -0800

    Add Singleton

---
+++ Robot Boxing/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private static GameController _instance;
+
+    public static GameController Instance { get { return _instance; } }
+
+
+    private void Start()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
