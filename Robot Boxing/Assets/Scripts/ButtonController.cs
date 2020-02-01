ad19d30a2d31ebda1f91b07927dce576032fcfbf
z99o <z99o@users.noreply.github.com>
2020-02-01 11:06:53 -0800

    Game Controller, Repair Menu, Fight Scene
    Added
    -Basic Repair Menu with Timer
    -Empty Fight Scene with button to go to Repair Menu
    -Very cool

---
+++ Robot Boxing/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    //Make sure to attach these Buttons in the Inspector
+    [Header("Stat Buttons")]
+    [SerializeField] private Button defenceButton;
+    [SerializeField] private Button sightButton;
+    [SerializeField] private Button grappleButton;
+    [SerializeField] private Button strengthButton;
+    [SerializeField] private Button speedButton;
+    [SerializeField] private Button pierceButton;
+    //Any buttoons that will open a menu
+    [Header("Menu Buttons")]
+    [SerializeField] private Button fightButton;
+    private GameController GameController;
+
+    void Start()
+    {
+        GameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        //adding event listeners cause we javascript now boys
+        defenceButton.onClick.AddListener(DefenceButton);
+        grappleButton.onClick.AddListener(GrappleButton);
+        sightButton.onClick.AddListener(SightButton);
+        strengthButton.onClick.AddListener(StrengthButton);
+        speedButton.onClick.AddListener(SpeedButton);
+        pierceButton.onClick.AddListener(PierceButton);
+        fightButton.onClick.AddListener(FightButton);
+    }
+
+    void Update(){
+        GameController.timer = GameController.timer -= Time.deltaTime;
+        //if timer reaches 0... start fight
+        if(GameController.timer <= 0)
+            FightButton();
+    }
+
+    /*
+        Each button has it's own function because it's easier and we might have certain buttons do more than one thing and other such things I guess, probably
+        could have had a listener function that took a parameter and put it in a case statement, but I want to keep it like this for now.
+    */
+    void DefenceButton()
+    {
+
+        GameController.ModifyDefence(1);
+    }
+    void GrappleButton()
+    {
+
+        GameController.ModifyGrapple(1);
+    }
+    void SightButton()
+    {
+
+        GameController.ModifySight(1);
+    }
+    void StrengthButton()
+    {
+
+        GameController.ModifyStrength(1);
+    }
+    void SpeedButton()
+    {
+
+        GameController.ModifySpeed(1);
+    }
+    void PierceButton()
+    {
+        
+        GameController.ModifyPierce(1);
+    }
+    void FightButton(){
+        //Load the fight
+        GameController.LoadFightScene();
+    }
+
+}
--- Robot Boxing/Assets/Scripts/GameController.cs
+++ Robot Boxing/Assets/Scripts/GameController.cs
@@ -1,12 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     private static GameController _instance;
 
     public static GameController Instance { get { return _instance; } }
+
+    [Header("Stats")]
+    public float defence = 0;
+    public float sight = 0;
+    public float grapple = 0;
+    public float strength = 0;
+    public float speed = 0;
+    public float pierce = 0;
+
+    [Header("Repair Timer")]
+    public float timer = 0; //for repair minigames
+    [SerializeField] private float repairTime; //time allowed for player to repair the robot
+    [SerializeField] private Text timerDisplay;
 
 
     private void Start()
@@ -19,4 +34,38 @@ public class GameController : MonoBehaviour
             DontDestroyOnLoad(this.gameObject);
         }
     }
+
+    private void Update(){
+        timerDisplay.text = "" + timer;
+    }
+
+    public void ModifyDefence(float n){
+        defence += n;
+    }
+    public void ModifyGrapple(float n){
+        grapple += n;
+    }
+    public void ModifySight(float n){
+        sight += n;
+    }
+    public void ModifyStrength(float n){
+        strength += n;
+    }
+    public void ModifySpeed(float n){
+        speed += n;
+    }
+    public void ModifyPierce(float n){
+        pierce += n;
+    }
+
+    public void LoadFightScene(){
+        timer = 0;
+        SceneManager.LoadScene("Fight Scene", LoadSceneMode.Single);
+    }
+    public void LoadRepairMenu(){
+        timer = repairTime; //set the timer
+        SceneManager.LoadScene("Repair Menu", LoadSceneMode.Single);
+        
+    }
+
 }
