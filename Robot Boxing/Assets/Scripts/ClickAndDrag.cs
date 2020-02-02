0aa6dfc0ebcddfe50904156a1f003e439d7864a8
Jason Stallkamp <stallkja@oregonstate.edu>
2020-02-01 17:20:24 -0800

    thing

---
+++ Robot Boxing/Assets/Scripts/ClickAndDrag.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ClickAndDrag : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public bool canMove = false;
+    private Rect location => new Rect(new Vector2(transform.position.x,transform.position.y) - this.GetComponent<RectTransform>().rect.size * .5f, this.GetComponent<RectTransform>().rect.size);
+    private bool isDragging;
+    private Vector3 lastMouseLocation;
+    Vector2 velocity;
+
+    public void SetDraggable(bool value)
+    {
+        this.canMove = value;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!canMove)
+            return;
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(location);
+            Debug.Log(Input.mousePosition);
+            if(location.Contains(Input.mousePosition))
+            {
+                isDragging = true;
+                lastMouseLocation = Input.mousePosition;
+            }
+            velocity = Vector2.zero;
+            
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+
+            velocity = (Input.mousePosition - lastMouseLocation) * (1.0f / Time.deltaTime);
+        }
+        if(isDragging)
+        {
+            
+            this.transform.position += Input.mousePosition - lastMouseLocation;
+            lastMouseLocation = Input.mousePosition;
+        }
+
+        velocity = velocity * .9f;
+        if (velocity.sqrMagnitude < .1)
+            velocity = Vector3.zero;
+        this.transform.position += Time.deltaTime * new Vector3(velocity.x, velocity.y, 0) ; 
+        
+    }
+}
---
+++ Robot Boxing/Assets/Scripts/EyeMiniGameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeMiniGameController : MonoBehaviour
+{
+
+    public float Radius;
+    public int NumbScrews;
+    public GameObject ScrewPrefab;
+    private int numUnscrewed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float degrees = 360.0f / (float)NumbScrews;
+        numUnscrewed = 0;
+        for ( int i = 0; i < NumbScrews; i++)
+        {
+            GameObject newScrew = GameObject.Instantiate(ScrewPrefab, transform);
+            newScrew.transform.position +=
+            Quaternion.Euler(0, 0, degrees * (float)i) * new Vector3(Radius, 0, 0);
+            newScrew.GetComponent<EyeMiniGameScrewController>().afterEvent.AddListener(ScrewUndone);
+            
+        }
+    }
+
+    void ScrewUndone()
+    {
+        numUnscrewed++;
+        if(numUnscrewed == NumbScrews)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
