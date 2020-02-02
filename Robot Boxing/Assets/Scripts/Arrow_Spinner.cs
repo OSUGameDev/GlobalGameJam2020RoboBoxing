6014591235ba13a3a619e2d03e9ee4b7a0cf62d6
z99o <z99o@users.noreply.github.com>
2020-02-01 23:15:37 -0800

    Revert "Merge branch 'master' into UI-Scaling-1"
    This reverts commit e0e0782b7c7f9abc9c053ef9540cc7b7e13a3fa8, reversing
    changes made to 1512c36cd2d79f5ac928ed315e6f73aa6ad3eeb9.

--- Robot Boxing/Assets/Scripts/Arrow_Spinner.cs
+++ Robot Boxing/Assets/Scripts/Arrow_Spinner.cs
@@ -14,10 +14,9 @@ public class Arrow_Spinner : MonoBehaviour
     {
         arrow = GameObject.Find("Arrow"); //build error happens if you do it during serialization
         float match2 = (float)GameObject.Find("GameController").GetComponent<GameController>().match + 1.0f;
-        GameObject.Find("GameController").GetComponentInChildren<Canvas>().enabled = false;
         Debug.Log(match2);
         Target_Pressure = 1000 * match2 + Random.Range(0, 11) * match2;
-        Click = Random.Range(0, 10 * match2) + 100;
+        Click = Random.Range(0, 10) + 10;
         Current_Increment = 0;
         arrow = GameObject.Find("Arrow");
     }
@@ -25,51 +24,13 @@ public class Arrow_Spinner : MonoBehaviour
     // Update is called once per frame
     public void Increment()
     {
-        time += Click;
+        Current_Increment += Click;
     }
 
     void Update()
     {
-        if (time > 0)
-        {
-            Current_Increment += 1;
-            time -= 1;
-            if (time > 25)
-            {
-                Current_Increment += 1;
-                time -= 1;
-                if (time > 100)
-                {
-                    Current_Increment += 1;
-                    time -= 1;
-                    if (time > 150)
-                    {
-                        Current_Increment += 1;
-                        time -= 1;
-                        if (time > 200)
-                        {
-                            Current_Increment += 1;
-                            time -= 1;
-                        }
-                    }
-                }
-            }
-            float test = (-360 * (Current_Increment / Target_Pressure));
-            Quaternion zero = Quaternion.Euler(0, 0, test);
-            arrow.transform.rotation = zero;
-        }
-
-        if (time <= 0)
-        {
-            float test = (360 * (Current_Increment / Target_Pressure));
-            float test2 = test % 360;
-            if (test2 >= 295 && test2 <= 332.5)
-            {
-                GameObject.Find("GameController").GetComponentInChildren<Canvas>().enabled = true;
-                GameController.Instance.player.ModifyArm(25);
-                UnityEngine.SceneManagement.SceneManager.LoadScene("Repair Menu", UnityEngine.SceneManagement.LoadSceneMode.Single);
-            }
-        }
-
+        float test = (-360 * (Current_Increment / Target_Pressure));
+        Quaternion zero = Quaternion.Euler(0, 0, test);
+        arrow.transform.rotation = zero;
     }
 }
